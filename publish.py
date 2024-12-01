6c651a42e10668a5940b6ecb042791115e23d0c6
eggplants <w10776e8w@yahoo.co.jp>
2024-12-01 11:27:30 +0900

    Fix ruff errors

--- publish.py
+++ publish.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 import json
-import os
+import shutil
 import subprocess
+from pathlib import Path
 from typing import TYPE_CHECKING
 from urllib.request import urlopen
 
@@ -22,23 +23,24 @@ def download_openapi_generator_jar(version: str) -> None:
         + ".jar"
     )
 
-    if os.path.exists("openapi-generator.jar"):
-        os.remove("openapi-generator.jar")
+    Path("openapi-generator.jar").unlink(missing_ok=True)
 
     print(download_url)
-    response = urlopen(download_url)
+    response = urlopen(download_url)  #  noqa: S310
 
-    if response.status != 200:
-        raise RuntimeError(f"{response.status}: {download_url}")
+    if response.status != 200:  # noqa: PLR2004
+        msg = f"{response.status}: {download_url}"
+        raise RuntimeError(msg)
 
     print("Downloading complete")
 
-    with open("openapi_generator_cli/openapi-generator.jar", "wb") as openapi_generator_jar:
+    with Path("openapi_generator_cli/openapi-generator.jar").open("wb") as openapi_generator_jar:
         openapi_generator_jar.write(response.read())
         openapi_generator_jar.close()
 
     updated_toml = (
-        open("pyproject.toml")
+        Path("pyproject.toml")  # noqa: SIM115
+        .open("r")
         .read()
         .replace(
             '[tool.poetry]\nversion = "0"',
@@ -46,7 +48,7 @@ def download_openapi_generator_jar(version: str) -> None:
             1,
         )
     )
-    with open("pyproject.toml", "w") as toml:
+    with Path("pyproject.toml").open("w") as toml:
         toml.write(updated_toml)
 
 
@@ -55,21 +57,23 @@ def get_available_versions() -> list[str]:
         MVN_BASE_URL + "/solrsearch/select?q=g:org.openapitools+AND+a:openapi-generator-cli&core=gav"
         "&start=0&rows=200"
     )
-    response = urlopen(mvn_url)
+    response = urlopen(mvn_url)  #  noqa: S310
     docs = json.loads(response.read())["response"]["docs"]
     return [doc["v"] for doc in docs]
 
 
 def get_published_vesions() -> KeysView[str]:
     pypi_url = "https://pypi.org/pypi/openapi-generator-cli/json"
-    response = urlopen(pypi_url)
+    response = urlopen(pypi_url)  #  noqa: S310
 
-    if response.status != 200:
-        raise RuntimeError(f"{response.status}: {pypi_url}")
+    if response.status != 200:  # noqa: PLR2004
+        msg = f"{response.status}: {pypi_url}"
+        raise RuntimeError(msg)
 
     published_releases = json.loads(response.read()).get("releases")
     if not isinstance(published_releases, dict):
-        raise TypeError(f"Expected dict, got {type(published_releases)}")
+        msg = f"Expected dict, got {type(published_releases)}"
+        raise TypeError(msg)
 
     return published_releases.keys()
 
@@ -81,9 +85,10 @@ def publish() -> None:
     if latest_version not in published_versions:
         print("Publishing version " + latest_version)
         download_openapi_generator_jar(latest_version)
-        subprocess.check_call("poetry build", shell=True)
-        subprocess.check_call("poetry publish -r testpypi", shell=True)
-        subprocess.check_call("poetry publish", shell=True)
+        poetry_path = shutil.which("poetry")
+        subprocess.check_call([poetry_path, "build"])  # noqa: S603
+        subprocess.check_call([poetry_path, "publish", "-r", "testpypi"])  # noqa: S603
+        subprocess.check_call([poetry_path, "publish"])  # noqa: S603
 
 
 if __name__ == "__main__":
