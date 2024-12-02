0cd51559f6cd9392fbe39ac270f0ffca85650b23
eggplants <w10776e8w@yahoo.co.jp>
2024-12-03 02:02:06 +0900

    Return result of jar execution in func

--- openapi_generator_cli/__init__.py
+++ openapi_generator_cli/__init__.py
@@ -1,3 +1,5 @@
+"""A Python wrapper for the OpenAPI Generator CLI."""
+
 from __future__ import annotations
 
 import importlib.resources
@@ -6,7 +8,19 @@ import subprocess
 import sys
 
 
-def run(args: list[str] | None = None) -> None:
+def run(args: list[str] | None = None) -> subprocess.CompletedProcess[bytes]:
+    """Run the OpenAPI Generator CLI with the given arguments.
+
+    Args:
+        args (list[str], optional):
+            The list of arguments to pass to the Open
+            API Generator CLI. If not provided, the CLI will
+            be run without any arguments.
+
+    Returns:
+        subprocess.CompletedProcess[bytes]: The result of running the OpenAPI Generator CLI.
+
+    """
     arguments = ["java"]
 
     java_opts = os.getenv("JAVA_OPTS")
@@ -18,13 +32,14 @@ def run(args: list[str] | None = None) -> None:
     jar_path = importlib.resources.files("openapi_generator_cli") / "openapi-generator.jar"
     arguments.append(str(jar_path))
 
-    if args and type(args) == list:
+    if args and isinstance(args, list):
         arguments.extend(args)
 
-    subprocess.call(arguments)  # noqa: S603
+    return subprocess.run(arguments, check=False)  # noqa: S603
 
 
 def cli() -> None:
+    """Run the OpenAPI Generator CLI with the arguments provided on the command line."""
     args = []
     if len(sys.argv) > 1:
         args = sys.argv[1:]
